48df444d9d2b1b931be7ca9c91bb9deefc35e9f1
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-02-14 14:21:32 -0500

    nested loops and loop issues

--- Week-05-More-Loops/SP25/LoopIssues.java
+++ Week-05-More-Loops/SP25/LoopIssues.java
@@ -12,5 +12,21 @@ public class LoopIssues
         }
         // num is out of scope here.
         // x = num;
+
+//        for (double value = 0.0; value != 1.0; value = value + 0.1)
+//        {
+//            System.out.println(value);
+//        }
+
+        for (double value = 0.0; value <= 1.0; value = value + 0.1)
+        {
+            System.out.printf("%.1f\n", value);
+        }
+
+        for (int value = 0; value != 10; value = value + 1)
+        {
+            System.out.println(value);
+        }
+
     }
 }
