413d14ccd31373d9e50f7bea535a2eee9994f1ab
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-08-25 16:06:15 -0400

    hello world examples

---
+++ Week-01-IO-Variables-Math/Code/FA25/HON/HelloHonors.java
@@ -0,0 +1,13 @@
+public class HelloHonors
+{
+    // this is the main method
+    // this is where the compiler knows to start
+    public static void main(String[] args)
+    {
+        /*
+        block comments give me a better way
+        of writing multi-line comments
+         */
+        System.out.println("Hello honors class!!!"); // i can also put a comment at the end of a line
+    }
+}
---
+++ Week-01-IO-Variables-Math/Code/FA25/Sec-02/HelloClass.java
@@ -0,0 +1,22 @@
+public class HelloClass // this is my class
+{
+    // this is the main method where
+    // my compiler knows where to start
+    public static void main(String[] args)
+    {
+        /*
+        it's a lot easier to
+        write multi-line comments
+        with a block comment
+         */
+        System.out.println("Hello and Welcome to Class!");
+        // variable creation and initialization can be split
+        int age;
+        //System.out.println(age);
+        age = 38;
+
+        // but often times we prefer to do them at the same time
+        int bobsAge = 55;
+        System.out.println(bobsAge);
+    }
+}
