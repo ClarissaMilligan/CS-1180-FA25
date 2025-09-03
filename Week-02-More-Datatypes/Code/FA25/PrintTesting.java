83f427a9a99c9bcf83fe8b7194fa00aef1e13ac8
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-09-03 14:26:25 -0400

    overflow

---
+++ Week-02-More-Datatypes/Code/FA25/PrintTesting.java
@@ -0,0 +1,19 @@
+public class PrintTesting
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hi");
+        System.out.print("How are you?");
+
+        System.out.println("\nHi");
+        System.out.println("How are you?");
+
+        int hugeNumber = 2147483647;
+        System.out.println(hugeNumber);
+        hugeNumber = hugeNumber + 1;
+        System.out.println(hugeNumber);
+        hugeNumber = hugeNumber * 3;
+        System.out.println(hugeNumber);
+
+    }
+}
