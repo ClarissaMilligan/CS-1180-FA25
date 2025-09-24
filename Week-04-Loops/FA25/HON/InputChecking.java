fed4d1b20ac9b6a8c5171d8b5988794bade4e32b
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-09-24 15:39:33 -0400

    input checking loops

--- Week-04-Loops/FA25/HON/InputChecking.java
+++ Week-04-Loops/FA25/HON/InputChecking.java
@@ -5,25 +5,55 @@ public class InputChecking
     public static void main(String[] args)
     {
         Scanner scnr = new Scanner(System.in);
+        boolean badUserInput = true;
+        int numFishies = 0;
 
-        System.out.println("How many fish do you have?");
-
-        while(!scnr.hasNextInt())
+        while (badUserInput)
         {
+            System.out.println("How many fish do you have?");
+
+            if (scnr.hasNextInt())
+            {
+                numFishies = scnr.nextInt();
+                if (numFishies >= 0)
+                {
+                    badUserInput = false;
+                }
+                else
+                {
+                    System.out.println("NO, BAD!");
+                }
+            }
+            else
+            {
+                System.out.println("That's not an int!!!");
+                System.out.println("Give me an int!!!");
+            }
             scnr.nextLine();
-            System.out.println("That's not an int!!!");
-            System.out.println("Give me an int!!!");
         }
-        int numFish = scnr.nextInt();
+        System.out.println("You have " + numFishies + " fishies!");
 
-        while (numFish < 0)
-        {
-            System.out.println("Give me positive number of fishies!!!");
-            numFish = scnr.nextInt();
-        }
 
-        System.out.println("You have " + numFish + " fishies!");
+        // original hasNextInt input looping
+//        System.out.println("How many fish do you have?");
+//
+//        while(!scnr.hasNextInt())
+//        {
+//            scnr.nextLine();
+//            System.out.println("That's not an int!!!");
+//            System.out.println("Give me an int!!!");
+//        }
+//        int numFish = scnr.nextInt();
+//
+//        while (numFish < 0)
+//        {
+//            System.out.println("Give me positive number of fishies!!!");
+//            numFish = scnr.nextInt();
+//        }
+//
+//        System.out.println("You have " + numFish + " fishies!");
 
+        // hasNextInt in if statement example
 //        if (scnr.hasNextInt())
 //        {
 //            System.out.println("fish");
--- Week-04-Loops/FA25/Sec-02/InputLoops.java
+++ Week-04-Loops/FA25/Sec-02/InputLoops.java
@@ -5,15 +5,31 @@ public class InputLoops
     public static void main(String[] args)
     {
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter the number of students in the class: ");
+        boolean badUserInput = true;
+        int numStudents = 0;
 
-        while (!scnr.hasNextInt())
+        while (badUserInput)
         {
-            scnr.nextLine();
-            System.out.println("That was not an integer!! Please enter an integer.");
             System.out.println("Enter the number of students in the class: ");
+            if (scnr.hasNextInt())
+            {
+                numStudents = scnr.nextInt();
+
+                if (numStudents >= 0)
+                {
+                    badUserInput = false;
+                }
+                else
+                {
+                    System.out.println("Input cannot be negative!! Please enter a positive integer.");
+                }
+            }
+            else
+            {
+                System.out.println("That was not an integer!! Please enter an integer.");
+            }
+            scnr.nextLine();
         }
-        int numStudents = scnr.nextInt();
         System.out.println("There are " + numStudents + " students in the class.");
     }
 }
