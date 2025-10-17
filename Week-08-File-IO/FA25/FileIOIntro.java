a5bcccf849f023500c28adba3da89a58a3faed2f
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-10-17 15:28:03 -0400

    Files, try/catch

--- Week-08-File-IO/FA25/FileIOIntro.java
+++ Week-08-File-IO/FA25/FileIOIntro.java
@@ -1,13 +1,65 @@
 import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class FileIOIntro
 {
     public static void main(String[] args) throws IOException
     {
-        FileWriter fw = new FileWriter("Hello.txt", true);
-        PrintWriter pw = new PrintWriter(fw);
-        pw.println("Hello all students!!");
-        pw.close();
-        fw.close();
+        boolean badInput = true;
+        Scanner scnr = new Scanner(System.in);
+
+//        for (int i = 0; i < 10; i++)
+//        {
+//            FileWriter fw = new FileWriter("Hello" + i + ".txt", true);
+//            PrintWriter pw = new PrintWriter(fw);
+//            pw.println("File #" + i);
+//            for (int j = 0; j < 10; j++)
+//            {
+//                pw.println(j);
+//            }
+//            pw.close();
+//            fw.close();
+//        }
+
+        while (badInput)
+        {
+            try
+            {
+                System.out.println("Type the name of the file you want to read from: ");
+                int fileNum = scnr.nextInt();
+                String fileName = "Hello" + fileNum + ".txt";
+
+                System.out.println(55 / fileNum);
+
+                File fileHandle = new File(fileName);
+                Scanner fileScnr = new Scanner(fileHandle);
+
+                badInput = false;
+
+                while (fileScnr.hasNextLine())
+                {
+                    String lineOne = fileScnr.nextLine();
+                    System.out.println(lineOne);
+                }
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                System.out.println("That file does not exist!");
+            }
+            catch (InputMismatchException ime)
+            {
+                System.out.println("You need to type in a number!");
+                scnr.nextLine();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Something unexpected occurred!");
+            }
+            finally
+            {
+                System.out.println("END OF LOOP");
+            }
+        }
     }
 }
--- Week-08-File-IO/FA25/HONFileIO.java
+++ Week-08-File-IO/FA25/HONFileIO.java
@@ -1,12 +1,75 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Random;
 
 public class HONFileIO
 {
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args) throws IOException
     {
-        PrintWriter pw = new PrintWriter("HON_Hello.txt");
-        pw.println("Hello students!!!!!!");
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scnr.nextLine();
+
+        int age = 0;
+        double toes = 0.0;
+        boolean invalidInput = true;
+
+        while (invalidInput)
+        {
+            try
+            {
+                System.out.println("How old are you?");
+                age = scnr.nextInt();
+                invalidInput = false;
+
+            } catch (InputMismatchException ime)
+            {
+                System.out.println("You need to enter an integer!");
+                scnr.nextLine();
+            }
+        }
+        invalidInput = true;
+        while (invalidInput)
+        {
+            try
+            {
+                System.out.println("How many toes do you have?");
+                toes = scnr.nextDouble();
+                invalidInput = false;
+            }
+            catch (Exception e)
+            {
+                System.out.println("You need to enter an number!");
+                scnr.nextLine();
+            }
+        }
+        String log = name + "\n" + age + "\n" + toes;
+        logData(log);
+
+        System.out.println("What is the number of the log you want to read?");
+        int fileNum = scnr.nextInt();
+        String fileName = "log-" + fileNum + ".txt";
+        File fileHandle = new File(fileName);
+        Scanner fileScnr = new Scanner(fileHandle);
+        int i = 0;
+        while (fileScnr.hasNextLine())
+        {
+            System.out.println("line #" + i + ": " + fileScnr.nextLine());
+            i++;
+        }
+
+    }
+
+    public static void logData(String data) throws IOException
+    {
+        Random rng = new Random();
+        int num = rng.nextInt(10);
+        System.out.println(num);
+        FileWriter fw = new FileWriter("log-" + num + ".txt", true);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(data);
         pw.close();
+        fw.close();
     }
 }
